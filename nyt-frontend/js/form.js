2ea2d0477c54369f4440d672bab4bbde98784434
Lexisnguyen <77212249+Lexisnguyen@users.noreply.github.com>
2021-07-05 23:44:24 +0700

    Add login + sign up

---
+++ nyt-frontend/js/form.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+    function ktramail() {
+        var reg = /^[A-Za-z0-9]{1,}(\@)((gmail)|(hotmail)|(yahoo))(\.com){1}$/;
+        var mail = $("#email").val();
+        if ($("#email").val() == '') {
+            return false;
+        }
+        if (reg.test(mail)) {
+            $("#errmail").html("");
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+    $("#continue").click(function() {
+        if (ktramail() == false) {
+            $("#errmail").html("Please enter your username or email address");
+        } else {
+            $(".access").css("display", "none");
+            $(".inp-pass").css("display", "block");
+            $(".agree").css("display", "block");
+            $(".button-box").css("display", "none");
+
+            $(".button-box-agree").css("display", "block");
+            $(".text-sign").css("display", "block");
+            $("footer").css("margin-top", "120px");
+            $("#edit").css("display", "block")
+
+        }
+
+    })
+
+
+})
